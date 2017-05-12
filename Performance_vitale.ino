59ade7c8c1cf8fc16ca6113c497c3c580ae8b674
unknown <nschrader@IF501-204-15.insa-lyon.fr>
2017-05-12 10:37:04 +0200

    CSV style output

--- Performance_vitale.ino
+++ Performance_vitale.ino
@@ -6,10 +6,10 @@ void setup() {
 }
 
 void loop() {
-  Serial.print(getCO2());
-  Serial.print('\n');
-  Serial.print(getHumidity());
-  Serial.print(getTemperature());
-  Serial.print('\n');
+  int ppm = getCO2();
+  float h = getHumidity();
+  float t = getTemperature();
+  String out = String(ppm) + ", " + String(h) + ", " + String(t) + '\n';
+  Serial.print(out);
   delay(UPDATE_INTERVAL);
 }
