b2655ad50306c7c538c1ac24a05adeb915d72b10
unknown <nschrader@IF501-204-08.insa-lyon.fr>
2017-05-19 09:47:45 +0200

    Parse GPGGA

--- GPS.ino
+++ GPS.ino
@@ -3,8 +3,13 @@
 #define GPS_RX 12
 #define GPS_TX 11
 
+#define BUFFER_LENGTH 64
+#define LAT_OFFSET 18
+#define LAT_LONG_LENGTH 24
+
 SoftwareSerial SoftSerial(GPS_RX, GPS_TX);
-char buffer[64];
+char buffer[BUFFER_LENGTH];
+char out[LAT_LONG_LENGTH+1];
 int count = 0;
 
 void beginGPS() {
@@ -16,20 +21,25 @@ char *readGPS() {
   while (SoftSerial.available())
   {
     buffer[count++] = SoftSerial.read();  
-    if (count == 64)
+    if (count == BUFFER_LENGTH)
       break;
     if (!strcmp(&buffer[count-2], "$$"))
       break;
   }
   count = 0;
-  if (!strncmp(buffer, "$GPGGA", 5))
-    return buffer;
-   else
+  if (!strncmp(buffer, "$GPGGA", 5)) {
+    parseGPGGA();
+    return out;
+  } else
     return NULL;
 }
-
 
 void clearBufferArray() {
   for (int i = 0; i < count; i++)
     buffer[i] = NULL;
 }
+
+void parseGPGGA() {
+  strncpy(out, buffer + LAT_OFFSET, LAT_LONG_LENGTH);
+}
+
