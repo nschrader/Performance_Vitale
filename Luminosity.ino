e6cfbbcd7eb21b72fe6ab3b476a7a3b426c6a2cf
Nick Schrader <nick.schrader@mailbox.org>
2017-05-24 17:28:33 +0200

    enhanced temperature and illuminance clculations

--- Luminosity.ino
+++ Luminosity.ino
@@ -2,7 +2,10 @@
 
 #define GAIN 64
 #define UW_CM2_PER_COUNT_X64 77
-#define LUX_PER_UW_CM2 7
+#define LUX_PER_UW_CM2 6.83
+
+#define Xe 0.3320
+#define Ye 0.1858
 
 void beginLuminosity() {
   Wire.begin();
@@ -13,7 +16,7 @@ int getColorTemperature() {
   GroveColorSensor colorSensor;
   double x, y;
   colorSensor.calculateCoordinate(&x, &y);
-  return calculateXTemperature(x);
+  return calculateTemperature(x, y);
 }
 
 int getBrightness() {
@@ -31,7 +34,8 @@ bool isValidLuminosity(int b, int c) {
   return b > 0 && b < 7000 && c > 1000 && c < 10000;
 }
 
-static float calculateXTemperature(float x) {
-  return 69129*x*x-90610*x+30594;
+static float calculateTemperature(float x, float y) {
+  float n = (x - Xe)/(y - Ye);
+  return -449*n*n*n + 3525*n*n - 6823.3*n + 5520.33;
 }
 
